ff0e8fc6059cf6e611e8b7db4e804c0a50b1a609
José Renato Vicente <61014145+JoseRenatoVicente@users.noreply.github.com>
2022-03-18 14:53:13 -0300

    Update script insert.sql

--- script insert.sql
+++ script insert.sql
@@ -1,12 +1,15 @@
+USE ControleAcademico1;
+GO
+
 INSERT INTO Aluno (CPF, RA, Nome, DataNascimento) VALUES
-('942.420.110-91', '21131', 'José Renato', '2004-03-22'),
+('942.420.110-91', '21131', 'JosÃ© Renato', '2004-03-22'),
 ('529.776.870-50', '21131', 'Pedro', '2004-03-22');
 
 INSERT INTO Disciplina (Sigla, Nome, CargaHoraria) VALUES
 ('002-IBD', 'Banco de dados', 200),
-('001-PW', 'Programação Web', 100),
+('001-PW', 'ProgramaÃ§Ã£o Web', 100),
 ('004-AS', 'Arquitetura de software', 100),
-('001-PM', 'Programação mobile', 100),
+('001-PM', 'ProgramaÃ§Ã£o mobile', 100),
 ('001-ES', 'Estrutura de dados', 100),
 ('003-AS', 'Analise Sistemas', 150);
 
@@ -17,10 +20,6 @@ INSERT INTO Matricula (Nota1, Nota2, NotaSubstutiva, Faltas, Ano, Semestre, Data
 /*REPROVADO POR FALTA*/
 INSERT INTO Matricula (Nota1, Nota2, NotaSubstutiva, Faltas, Ano, Semestre, DataMatricula, SiglaDisciplina, CPFAluno) VALUES
 (2, 5, 0, 50, 2022, 2, '2022-03-14', '001-ES', '942.420.110-91');
-
-/*Erro ao adicionar*/
-INSERT INTO Matricula (Nota1, Nota2, NotaSubstutiva, Faltas, Ano, Semestre, DataMatricula, SiglaDisciplina, CPFAluno) VALUES
-(2, 5, 0, 50, 2022, 2, '2022-03-14', '002-IBD', '942.420.110-91');
 
 /*REPROVADO POR NOTA*/
 INSERT INTO Matricula (Nota1, Nota2, NotaSubstutiva, Faltas, Ano, Semestre, DataMatricula, SiglaDisciplina, CPFAluno) VALUES
@@ -39,8 +38,7 @@ INSERT INTO Matricula (Nota1, Nota2, NotaSubstutiva, Faltas, Ano, Semestre, Data
 (0, 5, 7, 10, 2022, 2, '2022-03-14', '001-PW', '529.776.870-50');
 
 INSERT INTO Matricula (Nota1, Nota2, Faltas, Ano, Semestre, DataMatricula, SiglaDisciplina, CPFAluno) VALUES
-(10, 5, 50, 2022, 2, '2022-03-14', '002-IBD', '529.776.870-50'),
-(10, 7, 10, 2022, 2, '2022-03-14', '001-PW', '529.776.870-50');
+(10, 5, 50, 2022, 2, '2022-03-14', '002-IBD', '529.776.870-50');
 
 select * from Matricula
 
